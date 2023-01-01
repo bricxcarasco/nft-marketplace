fb9dabd7b67ef779f449add99d077398986e084d
bricxcarasco <bricxraincarasco21@gmail.com>
2023-01-01 23:29:46 +0800

    feat: created routes

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -46,36 +46,44 @@ function App() {
   }
 
   return (
-    <div>
-      <Navigation web3Handler={web3Handler} account={account} />
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo"/>
-              </a>
-              <h1 className= "mt-5">Dapp University Starter Kit</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-              </a>
-            </div>
-          </main>
+    <BrowserRouter>
+      <div>
+        <Navigation web3Handler={web3Handler} account={account} />
+        <Routes>
+          <Route path='/' element={} />
+          <Route path='/create' element={} />
+          <Route path='/my-listed-items' element={} />
+          <Route path='/my-purchases' element={} />
+        </Routes>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mx-auto mt-5">
+                <a
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={logo} className="App-logo" alt="logo"/>
+                </a>
+                <h1 className= "mt-5">Dapp University Starter Kit</h1>
+                <p>
+                  Edit <code>src/frontend/components/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                </a>
+              </div>
+            </main>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
